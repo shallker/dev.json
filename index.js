efb43dbe27f1cf2fe72161637e3bc948ac946003
Shallker Wang <shallker.wang@profero.com>
2013-09-01 17:10:55 +0800

    Version 1.0.0

---
+++ index.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "output": {
+    "EMERGENCY": true,
+    "ALERT": true,
+    "CRITICAL": true,
+    "ERROR": true,
+    "WARNING": true,
+    "NOTICE": true,
+    "INFO": true,
+    "DEBUG": true 
+  },
+  "throw": true
+}
